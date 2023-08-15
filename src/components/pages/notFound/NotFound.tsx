ad0ae104a81f8dbfe90b15041f16964dfd09487c
DartSter <kil-all@rambler.ru>
2023-08-15 21:47:17 +0300

    refactor: change not found page

--- src/components/pages/notFound/NotFound.tsx
+++ src/components/pages/notFound/NotFound.tsx
@@ -1,14 +1,15 @@
 import { ReactElement } from 'react';
 import { NavLink } from 'react-router-dom';
 import './notFound.css';
-import pageError from '../../../assets/icons/404.png';
+import pageError from '../../../assets/icons/404.jpg';
 
 export default function NotFound(): ReactElement {
   return (
     <div className='not_found-container'>
       <img className='not_found-img' src={pageError} alt='404' />
-      <h1>Error 404! Page not found!</h1>
-      <h2>Try another url</h2>
+      <h1>404!</h1>
+      <h2>Page not found!</h2>
+      <h2>Try again or...</h2>
       <NavLink className='not_found-nav_link' to='/'>
         Go to Main
       </NavLink>
