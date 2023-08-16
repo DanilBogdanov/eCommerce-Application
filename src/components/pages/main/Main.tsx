311aff1173cd202ad4d08595c7396e97e839893a
DartSter <kil-all@rambler.ru>
2023-08-16 12:12:36 +0300

    feat: add styles

--- src/components/pages/main/Main.tsx
+++ src/components/pages/main/Main.tsx
@@ -2,9 +2,13 @@ import { ReactElement } from 'react';
 
 export default function Main(): ReactElement {
   return (
-    <>
+    <main>
       <h2>Main</h2>
-      <img src='/img/inProgress.png' alt='in building' />
-    </>
+      <img
+        src='/img/inProgress.png'
+        alt='in building'
+        style={{ width: '100%' }}
+      />
+    </main>
   );
 }
