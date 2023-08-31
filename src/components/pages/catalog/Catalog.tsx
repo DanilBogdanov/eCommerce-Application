cc4d1f8151c84d8acc86b41de5f73bd52bda302e
DartSter <kil-all@rambler.ru>
2023-08-31 22:54:55 +0300

    feat: add navigation

--- src/components/pages/catalog/Catalog.tsx
+++ src/components/pages/catalog/Catalog.tsx
@@ -1,8 +1,9 @@
 import { ReactElement, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useSearchParams } from 'react-router-dom';
 import { api } from '../../../api/api';
 import { ApiResponse, Category, ProductsResponse } from '../../../types/api';
 import './catalog.css';
+import { DEFAULT_LIMIT_PER_PAGE } from '../../../types/constants';
 
 export default function Catalog(): ReactElement {
   const [categories, setCategories] = useState<ApiResponse<Category[]> | null>(
@@ -10,8 +11,12 @@ export default function Catalog(): ReactElement {
   );
   const [products, setProducts] =
     useState<ApiResponse<ProductsResponse> | null>(null);
+  const [search, setSearch] = useState<string | null>(null);
+  const [pageLimit, setPageLimit] = useState(DEFAULT_LIMIT_PER_PAGE);
+  const [currentPage] = useState(0);
+  const [currentCategory, setCurrentCategory] = useState('All stuff');
+  const [searchParams] = useSearchParams();
 
-  const [pageLimit, setPageLimit] = useState(10);
   const location = useLocation();
 
   useEffect(() => {
@@ -19,41 +24,94 @@ export default function Catalog(): ReactElement {
       const categoriesResponse = await api.catalog.getCategories();
       const productsResponse = await api.catalog.getProducts({
         limit: pageLimit,
+        page: currentPage,
       });
       setCategories(categoriesResponse);
       setProducts(productsResponse);
     };
     callData();
-  }, [pageLimit]);
+  }, [pageLimit, currentPage]);
+
+  useEffect(() => {
+    setSearch(searchParams.get('search'));
+  }, [searchParams]);
+
+  useEffect(() => {
+    const updateProducts = async () => {
+      const pathArray = location.pathname.split('/');
+      const lastNest = pathArray[pathArray.length - 1];
+      if (lastNest !== 'catalog' && categories && categories.data) {
+        const filteredCategoryByUrl = categories.data.filter(
+          (item) => item.name.toLowerCase() === lastNest,
+        );
+        setCurrentCategory(lastNest);
+        const categoryId = filteredCategoryByUrl[0].id;
+        const newProducts = await api.catalog.getProducts({
+          limit: pageLimit,
+          categoryId,
+          page: currentPage,
+        });
+        setProducts(newProducts);
+      } else {
+        const newProducts = await api.catalog.getProducts({
+          limit: pageLimit,
+          page: currentPage,
+        });
+        setProducts(newProducts);
+        setCurrentCategory('All Staff');
+      }
+    };
+    updateProducts();
+  }, [location, pageLimit, currentPage, categories]);
 
   return (
     <main className='catalog'>
       <div className='sidebar'>
         {categories &&
           categories.data &&
-          categories?.data?.map((item) => (
-            <button type='button' key={item.key} id={item.id}>
-              {item.name}
-            </button>
-          ))}
+          categories?.data?.map((item) => {
+            const categoryName = item.name.toLowerCase();
+            return (
+              <NavLink
+                to={`/catalog/${categoryName}`}
+                key={item.key}
+                id={item.id}
+              >
+                {item.name}
+              </NavLink>
+            );
+          })}
       </div>
       <div className='products-main'>
         <div className='products-main__header'>
           <div className='products-main__breadcrumbs'>{location.pathname}</div>
-          <div className='products-main__category-name'>Products</div>
+          <div className='products-main__category-name'>
+            {search
+              ? `Result of search: ${search}`
+              : currentCategory.toUpperCase()}
+          </div>
           <div className='products-main__pagination'>
             {products?.data?.currentPage}
             {products?.data?.totalPage}
           </div>
           <div className='products-main__page-limit-settings'>
-            <button type='button' onClick={() => setPageLimit(10)}>
-              10
+            <button
+              type='button'
+              onClick={() => setPageLimit(DEFAULT_LIMIT_PER_PAGE)}
+            >
+              {DEFAULT_LIMIT_PER_PAGE}
             </button>
-            <button type='button' onClick={() => setPageLimit(20)}>
-              20
+            <button
+              type='button'
+              onClick={() => setPageLimit(DEFAULT_LIMIT_PER_PAGE * 2)}
+            >
+              {DEFAULT_LIMIT_PER_PAGE * 2}
             </button>
-            <button type='button' onClick={() => setPageLimit(50)}>
-              50
+            <button
+              type='button'
+              onClick={() => setPageLimit(DEFAULT_LIMIT_PER_PAGE * 5)}
+            >
+              {DEFAULT_LIMIT_PER_PAGE * 5}
             </button>
           </div>
         </div>
