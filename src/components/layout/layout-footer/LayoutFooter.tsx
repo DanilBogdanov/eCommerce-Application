8a176d7c2952ad5ee840d6664b19eb4163fa80cb
DartSter <kil-all@rambler.ru>
2023-09-04 23:49:24 +0300

    feat: add test for componets, fix broken test

--- src/components/layout/layout-footer/LayoutFooter.tsx
+++ src/components/layout/layout-footer/LayoutFooter.tsx
@@ -8,14 +8,28 @@ export default function LayoutFooter(): ReactElement {
     <footer className='footer' data-testid='layout-footer'>
       <div className='footer__container'>
         <div className='footer__authors'>
-          <NavLink to='https://github.com/danilbogdanov'>
-            <img src='/icons/footer/github.svg' height={25} alt='user' />
+          <NavLink
+            to='https://github.com/danilbogdanov'
+            data-testid='danilbogdanov'
+          >
+            <img
+              src='/icons/footer/github.svg'
+              height={25}
+              alt='danilbogdanov'
+            />
           </NavLink>
-          <NavLink to='https://github.com/dartster'>
-            <img src='/icons/footer/github.svg' height={25} alt='user' />
+          <NavLink to='https://github.com/dartster' data-testid='dartster'>
+            <img src='/icons/footer/github.svg' height={25} alt='dartster' />
           </NavLink>
-          <NavLink to='https://github.com/natteskrekkk'>
-            <img src='/icons/footer/github.svg' height={25} alt='user' />
+          <NavLink
+            to='https://github.com/natteskrekkk'
+            data-testid='natteskrekkk'
+          >
+            <img
+              src='/icons/footer/github.svg'
+              height={25}
+              alt='natteskrekkk'
+            />
           </NavLink>
         </div>
         <p className='footer__info'>Created by Winners Team 2023</p>
